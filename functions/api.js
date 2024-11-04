7f88ca414ea4ef587d10794d85367a42c57af099
kozzztia <kozzztia@gmail.com>
2024-11-04 10:21:09 +0200

    first commit

---
+++ functions/api.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const axios = require("axios");
+const serverless = require("serverless-http");
+const app = express();
+app.use(express.json());
+
+app.get('/api/users', async (req, res) => {
+    try{
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error fetching users' });
+    }
+});
+app.get('/api/store', async (req, res) => {
+    try{
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error fetching store' });
+    }
+});
+
+
+// const PORT = process.env.PORT || 5555;
+// app.listen(PORT, () => {
+//     console.log(`Server listening on port ${PORT}`);
+// });
+module.exports.handler = serverless(app);
