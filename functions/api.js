e187679886d3cea5b138d9b2585b9d35566c3837
kozzztia <kozzztia@gmail.com>
2024-11-04 14:02:04 +0200

    next try as function

--- functions/api.js
+++ functions/api.js
@@ -1,27 +1,17 @@
-const express = require("express");
-const axios = require("axios");
-const serverless = require("serverless-http");
-const app = express();
-app.use(express.json());
-
-app.get('/api/users', async (req, res) => {
-    try{
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-    res.json(response.data);
+exports.handler = async (event, context) => {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await response.json();
+        console.log(data);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        };
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Error fetching users' });
+        return {
+            statusCode: 500,
+            body: error.toString()
+        };
     }
-});
-app.get('/api/store', async (req, res) => {
-    try{
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-    res.json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error fetching store' });
-    }
-});
-
-
-module.exports.handler = serverless(app);
+}
